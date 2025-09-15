fe5fe47bfb09c770c5fff781758bc4e820843bbd
Siu Sab <san24022005@example.com>
2025-09-15 12:52:41 +0700

    fisrt commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Shopee Clone BEM</title>
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/fonts/font-awesome/css/all.min.css">
+    <link rel="stylesheet" href="./assets/fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php 
+        require './widget/top.php'; 
+    ?>
+</body>
+</html>
---
+++ widget/top.php
@@ -0,0 +1,73 @@
+<div class="shopee__top shopee__top--sticky">
+    <div class="container navbar">
+        <div class="navbar__wrapper">
+            <a href="https://banhang.shopee.vn/" class="navbar__item">Kênh Người Bán</a>
+            <a href="https://shopee.vn/seller/signup" class="navbar__item">Trở thành Người Bán Shopee</a>
+            <a href="https://shopee.vn/web" class="navbar__item">
+                Tải ứng dụng
+                <div class="sub-navbar sub-navbar-dw">
+                    <img src="./assets/img/qr_code.png" alt="QR Code" class="sub-navbar-dw__qr">
+                    <div class="sub-navbar-dw__app">
+                        <div class="sub-navbar-dw__item-app">
+                            <img src="./assets/img/apple-icon.png" class="sub-navbar__img" alt="Icon Apple">
+                        </div>
+                        <div class="sub-navbar-dw__item-app">
+                            <img src="./assets/img/google-play-icon.png" class="sub-navbar__img" alt="Icon Google Play">
+                        </div>
+                        <div class="sub-navbar-dw__item-app">
+                            <img src="./assets/img/app-galler-icon.png" class="sub-navbar__img" alt="Icon App Galler">
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="" class="navbar__item not-hover">Kết nối</a>
+            <div class="navbar__social">
+                <a href="https://www.facebook.com/ShopeeVN" class="navbar__social-link">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a href="https://www.instagram.com/Shopee_VN" class="navbar__social-link">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+        <div class="navbar__links">
+            <a href="" class="navbar__link__item">
+                <i class="navbar__icon ti-bell navbar__notification-icon"></i>
+                Thông Báo
+            </a>
+            <a href="https://help.shopee.vn/portal/4/vn/s" class="navbar__link__item">
+                <i class="navbar__icon ti-help-alt"></i> 
+                Hỗ Trợ
+            </a>
+            <a href="" class="navbar__link__item navbar__language">
+                <i class="navbar__icon ti-world"></i>
+                Tiếng Việt
+                <i class="navbar__icon ti-angle-down .angle-down-icon"></i>
+            </a>
+            <div class="navbar__authentication navbar__link__item">
+                <a href="" class="navbar__link__item navbar__sign-in">Đăng Ký</a>
+                <a href="" class="navbar__link__item navbar__log-in">Đăng Nhập</a>
+            </div>           
+        </div>
+    </div>
+    <div class="cotainer header-with-search">
+        <div class="container__wrapper header-with-search__wrapper">
+            <a href="https://shopee.vn/" class="header-with-search__logo-link">
+                <img src="./assets/img/logo.png" alt="Shopee Logo" class="header-with-search__logo">
+            </a>
+            <div class="header-with-search__search">
+                <input type="text" class="header-with-search__input" placeholder="Tìm kiếm trên Shopee">
+                <button class="header-with-search__btn">
+                    <i class="header-with-search__icon ti-search"></i>
+                </button>
+            </div>
+            <div class="header-with-search__cart">
+                <i class="header-with-search__cart-icon ti-shopping-cart"></i>
+                <span class="header-with-search__cart-text">Giỏ Hàng</span>
+                <span class="header-with-search__cart-notice">0</span>
+            </div>
+        </div>
+    </div>
+    
+</div>
+    
