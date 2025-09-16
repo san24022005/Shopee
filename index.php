f9714ab6b9263328e3e097d7a7d8a1e6715f77b1
Siu Sab <san24022005@example.com>
2025-09-16 22:45:05 +0700

    add haeder

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <title>Shopee Clone BEM</title>
+    <title>Shopee Việt Nam | Mua và Bán</title>
     <link rel="stylesheet" href="./assets/css/base.css">
     <link rel="stylesheet" href="./assets/css/main.css">
     <link rel="stylesheet" href="./assets/fonts/font-awesome/css/all.min.css">
     <link rel="stylesheet" href="./assets/fonts/themify-icons/themify-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <?php 
-        require './widget/top.php'; 
-    ?>
+    <div class="main">
+        <?php 
+            require './widget/top.php'; 
+        ?>
+    </div>
 </body>
 </html>
