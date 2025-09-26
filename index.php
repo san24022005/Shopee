1b2745dd35682708ca3d350322c07721b7ec95f2
Siu Sab <san24022005@example.com>
2025-09-26 21:26:39 +0700

    fix css

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="./assets/fonts/themify-icons/themify-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" type="image/png" href="./assets/img/favicon.jpg">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <div class="main">
