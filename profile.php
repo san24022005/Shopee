071f24226b6c4a3b1b1ab89b1433ffad12f26131
Siu Sab <san24022005@example.com>
2025-12-09 22:40:01 +0700

    sữa lỗi font icon

--- profile.php
+++ profile.php
@@ -4,8 +4,10 @@
 require 'connect.php'; 
 session_start();
 
+$user_id = $_SESSION['khachhang_id'] ?? 0;
+
 // --- Kiểm tra Đăng nhập và Lấy User ID ---
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['khachhang_id'])) {
  header("Location: login.php");
 exit();
     // Dùng cho mục đích DEBUG/TEST:
@@ -141,8 +143,12 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hồ Sơ Của Tôi - Shopee Clone</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/base.css"> 
+    <link rel="stylesheet" href="./assets/css/base.css">
     <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/fonts/font-awesome/css/all.min.css">
+    <link rel="stylesheet" href="./assets/fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
     <link rel="stylesheet" href="./assets/css/profile.css"> 
 </head>
 <style>
@@ -155,7 +161,12 @@ $conn->close();
     }
 </style>
 <body>
-    <?php require './widget/top.php'; ?>
+    <div class="shopee__top shopee__top--sticky">
+            <?php
+                require './widget/top.php';
+                require './widget/header.php';
+            ?>
+        </div>
 
     <div class="main-body-container">
         
